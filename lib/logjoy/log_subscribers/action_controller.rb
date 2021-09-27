8569bfb7540a1eb428b4a2eea213b6ac4d75d0fd
Pat McGee <patmcgee331@gmail.com>
2021-09-27 17:49:32 -0400

    first pass at action_controller log subscriber

--- lib/logjoy/log_subscribers/action_controller.rb
+++ lib/logjoy/log_subscribers/action_controller.rb
@@ -9,35 +9,119 @@ module Logjoy
 
     class ActionController < ActiveSupport::LogSubscriber
       def start_processing(event)
-        binding.pry
+        info do
+          payload = event.payload
+
+          log = payload.slice(:controller, :action, :format, :method, :path)
+          log[:params] = cleanup_params(payload[:params])
+          log[:request_id] = payload[:request].request_id
+          log[:event] = event.name
+          log
+        end
       end
 
       def process_action(event)
-        binding.pry
+        info do
+          payload = event.payload
+
+          log = payload.slice(:controller, :action, :format, :method, :path, :status)
+          log[:view_runtime] = payload[:view_runtime].round
+          log[:db_runtime] = payload[:view_runtime].round
+          log[:duration] = event.duration.round
+          log[:params] = cleanup_params(payload[:params])
+          log[:request_id] = payload[:request].request_id
+          log[:event] = event.name
+          log[:allocations] = event.allocations
+
+          if log[:status].nil? && (exception_class_name = payload[:exception]&.first)
+            log[:exception] = exception_class_name
+            log[:status] = ::ActionDispatch::ExceptionWrapper.status_code_for_exception(exception_class_name)
+          end
+
+          log
+        end
       end
 
       def halted_callback(event)
-        binding.pry
+        debug do
+          {
+            filter: event.payload[:filter].inspect,
+            event: event.name
+          }
+        end
       end
 
       def send_file(event)
-        binding.pry
+        info do
+          {
+            path: event.payload[:path],
+            duration: event.duration.round,
+            event: event.name
+          }
+        end
       end
 
       def redirect_to(event)
-        binding.pry
+        info do
+          payload = event.payload
+
+          log = payload.slice(:status, :location)
+          log[:request_id] = payload[:request].request_id
+          log[:event] = event.name
+          log
+        end
       end
 
       def send_data(event)
-        binding.pry
+        info do
+          {
+            filename: event.payload[:filename],
+            duration: event.duration.round,
+            event: event.name
+          }
+        end
       end
 
       def unpermitted_parameters(event)
-        binding.pry
+        debug do
+          {
+            keys: event[:payload].keys,
+            event: event.name
+          }
+        end
+      end
+
+      %w[
+        write_fragment
+        read_fragment
+        exist_fragment?
+        expire_fragment
+        expire_page
+        write_page
+      ].each do |method|
+        class_eval <<-METHOD, __FILE__, __LINE__ + 1
+        def #{method}(event)
+          return unless ActionController::Base.enable_fragment_cache_logging
+
+          info do
+            {
+              key: ActiveSupport::Cache.expand_cache_key(event.payload[:key] || event.payload[:path]),
+              duration: event.duration.round,
+              event: event.name
+            }
+          end
+        end
+        METHOD
       end
 
       def logger
         ::ActionController::Base.logger
+      end
+
+      private
+
+      def cleanup_params(params)
+        params.except(*::ActionController::LogSubscriber::INTERNAL_PARAMS)
       end
     end
   end
--- logjoy.gemspec
+++ logjoy.gemspec
@@ -11,7 +11,7 @@ Gem::Specification.new do |spec|
   spec.summary       = 'Alternative set of LogSubscriber classes for Rails for more structured JSON logs'
   spec.homepage      = 'https://github.com/pathouse/logjoy'
   spec.license       = 'MIT'
-  spec.required_ruby_version = Gem::Requirement.new('>= 2.7.0')
+  spec.required_ruby_version = Gem::Requirement.new('>= 2.4.0')
 
   spec.metadata['homepage_uri'] = spec.homepage
   spec.metadata['source_code_uri'] = spec.homepage
